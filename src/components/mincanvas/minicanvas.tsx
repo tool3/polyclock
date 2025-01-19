29ec78d0f6a1cb74fd973be939f9a12abcbadb7a
Tal Hayut <talhayut.dev@gmail.com>
2025-01-19 08:38:54 +0200

    chore: vfx

--- src/components/mincanvas/minicanvas.tsx
+++ src/components/mincanvas/minicanvas.tsx
@@ -1,12 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import { Environment, OrbitControls, Stats } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import {
-  Bloom,
-  EffectComposer,
-  Scanline,
-  Vignette
-} from '@react-three/postprocessing'
+import { Bloom, EffectComposer, Scanline } from '@react-three/postprocessing'
 import { button, Leva, useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 import { ReactNode, Suspense, useRef, useState } from 'react'
@@ -90,9 +85,8 @@ export default function CanvasWithModel({
               height={1024}
               width={1024}
             />
-            {/* <Noise opacity={0.05} /> */}
+            <Noise opacity={0.05} />
             <Scanline opacity={0.1} />
-            <Vignette darkness={0.6} offset={0.3} />
           </EffectComposer>
         ) : null}
       </Canvas>
