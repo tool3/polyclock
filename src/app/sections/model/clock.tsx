ce33aeaed2f656a4ea4bffc957df27f55f1569a2
Tal Hayut <talhayut.dev@gmail.com>
2025-01-19 23:06:41 +0200

    chore: switch to setInterval

--- src/app/sections/model/clock.tsx
+++ src/app/sections/model/clock.tsx
@@ -71,12 +71,15 @@ export default function Clock() {
   )
 
   useEffect(() => {
-    const currentTime = new Date()
-    const hours = currentTime.getHours().toString().padStart(2, '0')
-    const minutes = currentTime.getMinutes().toString().padStart(2, '0')
-    const seconds = currentTime.getSeconds().toString().padStart(2, '0')
-    setTime({ hours, minutes, seconds })
-    setDate(currentTime)
+    const interval = setInterval(() => {
+      const currentTime = new Date()
+      const hours = currentTime.getHours().toString().padStart(2, '0')
+      const minutes = currentTime.getMinutes().toString().padStart(2, '0')
+      const seconds = currentTime.getSeconds().toString().padStart(2, '0')
+      setTime({ hours, minutes, seconds })
+      setDate(currentTime)
+    }, 5)
+    return () => clearInterval(interval)
   }, [date])
 
   const [first, second] = time.hours
