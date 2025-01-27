c1781915f126100d7ef7371b01f2fcf84fe1224a
Tal Hayut <talhayut.dev@gmail.com>
2025-01-27 15:23:34 +0200

    chore: black theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
 }
 
 export const viewport: Viewport = {
-  themeColor: '#722c0b'
+  themeColor: '#000000'
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
