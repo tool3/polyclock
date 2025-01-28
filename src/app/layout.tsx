17918e448f5e3d8d6e2f5c1d584bb2abd0f30c4d
Tal Hayut <talhayut.dev@gmail.com>
2025-01-28 17:34:05 +0200

    chore: android fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,12 @@ export const metadata: Metadata = {
 }
 
 export const viewport: Viewport = {
-  themeColor: '#000000'
+  themeColor: '#000000',
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
